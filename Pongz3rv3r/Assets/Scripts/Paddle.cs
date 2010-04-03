16550387323744b9d5fbf29d9c2d7b4d18e1569d
Michael Harrington <kibiz0r@gmail.com>
2010-04-03 17:24:52 -0400

    Added stuff.

---
+++ Pongz3rv3r/Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class Paddle : MonoBehaviour
+{
+    public Guid Guid;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Pongz3rv3r/Assets/Scripts/Server.cs
+++ Pongz3rv3r/Assets/Scripts/Server.cs
@@ -1,15 +1,36 @@
 using UnityEngine;
 using System.Collections;
+using System;
+using System.Collections.Generic;
 
 public class Server : MonoBehaviour
 {
+    public GameObject PlayerGroup;
+    public GameObject Paddle;
+    public Dictionary<Guid, GameObject> PaddleDictionary = new Dictionary<Guid, GameObject>();
+
     // Use this for initialization
     void Start()
     {
+        NetworkConnectionError error = Network.InitializeServer(32, 25000);
+        if (error != NetworkConnectionError.NoError)
+        {
+            Debug.Log(error);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    void OnPlayerConnected(NetworkPlayer player)
+    {
+        GameObject playerGroup = Network.Instantiate(PlayerGroup, Vector3.zero, Quaternion.identity, 0) as GameObject;
+        GameObject paddle = Network.Instantiate(Paddle, Vector3.zero, Quaternion.identity, 0) as GameObject;
+        Guid guid = Guid.NewGuid();
+        paddle.GetComponent<Paddle>().Guid = guid;
+        PaddleDictionary.Add(guid, paddle);
+        networkView.RPC("MakeController", player, guid);
     }
 }
